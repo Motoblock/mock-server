cd419fce5e502a5253d018467d2f5141045d5d6b
motoblock <milasasha@gmail.com>
2023-04-01 19:07:44 +0300

    fix:  file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const jsonServer = require('json-server');
 const catalog = require('./db/catData.json');
 
@@ -10,25 +9,29 @@ const db = {
 
 const server = jsonServer.create();
 const router = jsonServer.router(db);
-
 server.use(jsonServer.defaults({ static: __dirname + '/public' }));
 
+// Set readOnly
 server.use(jsonServer.defaults({ readOnly: true }));
 
+// Avoid CORS issue
 server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-server.use(  
+// Add this before server.use(router)
+server.use(
+  // Add custom route here if needed
   jsonServer.rewriter({ '/api/*': '/$1', })
 );
 
 server.use(router);
 
 server.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
-  });
+  console.log('Server is running on port', PORT);
+});
 
-  module.exports = server;
+// Export the Server API
+module.exports = server;
