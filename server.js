2dfb80b5ad68e71e211eda2f2a75e5187e57fce3
motoblock <milasasha@gmail.com>
2023-04-01 18:22:03 +0300

    init: create server

---
+++ server.js
@@ -0,0 +1,35 @@
+
+const jsonServer = require('json-server');
+const catalog = require('./db/catData.json');
+
+const PORT = 3000;
+
+const db = {
+  catalog: catalog
+};
+
+const server = jsonServer.create();
+const router = jsonServer.router(db);
+
+server.use(jsonServer.defaults({ static: __dirname + '/public' }));
+
+server.use(jsonServer.defaults({ readOnly: true }));
+
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// server.use(
+//   // Add custom route here if needed
+//   jsonServer.rewriter({ '/api/*': '/$1', })
+// );
+
+server.use(router);
+
+server.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+  });
+
+  module.exports = server;
